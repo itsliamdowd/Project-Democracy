41b9a02b491e4854a8d04055f7583d3392ad7e8f
SkiingIsFun123 <101684827+SkiingIsFun123@users.noreply.github.com>
2022-08-25 22:46:51 -0700

    Initial commit

--- Democracy/ViewController.swift
+++ Democracy/ViewController.swift
@@ -6,14 +6,91 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected a button...")
+        print(data[indexPath.row])
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = stateElections.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+}
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var data = ["Loading...", "Loading...", "Loading...", "Loading...", "Loading..."]
+    
+    
+    let locationManager = CLLocationManager()
+    @IBOutlet var stateElections: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        stateElections.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        stateElections.dataSource = self
+        stateElections.delegate = self
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location: CLLocation = manager.location else { return }
+        fetchCityAndCountry(from: location) { city, country, error in
+            guard let city = city, let country = country, error == nil else { return }
+            print(city + ", " + country)
+            var citycountrydata = city + ", " + country
+            var stateElectionsArray = [String].self()
+            var countyElectionsArray = [String].self()
+            var localElectionsArray = [String].self()
+            var federalElectionsArray = [String].self()
+            switch citycountrydata {
+                case "San Juan Capistrano, United States":
+                    print("San Juan Capistrano")
+                    self.data = ["State Senate", "State Assembly Member", "Governor", "Lieutenant Governor", "Secretary of State", "Controller", "Treasurer", "Attorney General", "Insurance Commissioner", "Member of State Board of Equalization", "State Superintendent of Public Instruction"]
+                    print("refreshed")
+                    DispatchQueue.main.async { self.stateElections.reloadData() }
+                    //"U.S. Senate", "U.S. Representative in Congress",
+                case "Dana Point, United States":
+                    print("Dana Point")
+                case "Laguna Niguel, United States":
+                    print("Laguna Niguel")
+                case "Laguna Beach, United States":
+                    print("Laguna Beach")
+                case "San Clemente, United States":
+                    print("San Clemente")
+                case "Irvine, United States":
+                    print("Irvine")
+                case "Orange, United States":
+                    print("Irvine")
+                default:
+                    print("Error")
+            }
+        }
+    }
+    
+    func fetchCityAndCountry(from location: CLLocation, completion: @escaping (_ city: String?, _ country:  String?, _ error: Error?) -> ()) {
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            completion(placemarks?.first?.locality,
+                       placemarks?.first?.country,
+                       error)
+        }
+    }
+
+}
