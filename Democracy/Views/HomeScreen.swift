e0069331b4bb574bdace1d6aed4054bfafb14403
Jevon Mao <woodburyjevonmao@gmail.com>
2022-09-22 18:30:29 -0700

    Refactor API and JSON parsing

--- Democracy/Views/HomeScreen.swift
+++ Democracy/Views/HomeScreen.swift
@@ -9,6 +9,7 @@ import UIKit
 import MapKit
 import CoreLocation
 import Foundation
+import SwiftyJSON
 
 extension HomeScreen: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -62,81 +63,7 @@ class HomeScreen: UIViewController {
         conversationButton.layer.cornerRadius = 15
         conversationButton.isHidden = true
         print(self.homescreendata)
-        if UserDefaults.standard.string(forKey: "longitude") != nil && UserDefaults.standard.string(forKey: "latitude") != nil && self.homescreendata.count == 0 {
-            let latitude = UserDefaults.standard.string(forKey: "latitude")
-            let longitude = UserDefaults.standard.string(forKey: "longitude")
-            let location = CLLocationCoordinate2D(latitude: CLLocationDegrees(Double(latitude!)!),
-                                                  longitude: CLLocationDegrees(Double(longitude!)!))
-            let request = Endpoint.getAPI(from: .ballotpediaElectionInfo(location: location))
-
-            URLSession.shared.codableTask(with: request) {[weak self] model in
-                typealias c = Constants.JSON
-                let electionsJson = model?[c.data][c.elections].array ?? []
-                let elections: [BallotpediaElection] = electionsJson.compactMap {election in
-                    let formatter = DateFormatter()
-                    formatter.dateFormat = "yyyy-MM-dd"
-                    let date = formatter.date(from: election[c.date].stringValue)
-
-                    let districtsJson = election[c.districts].array ?? []
-                    let districts: [BallotpediaElection.District] = districtsJson.compactMap {
-                        let racesJson = $0[c.races].array ?? []
-                        let races: [BallotpediaElection.Race] = racesJson.compactMap {
-                            let rawLevel = $0[c.office][c.level].stringValue
-                            guard let level = BallotpediaElection.ElectionLevel(rawValue: rawLevel.lowercased())
-                            else {
-                                return nil
-                            }
-
-                            let name = $0[c.office][c.name].stringValue
-                            let candidatesJson = $0[c.candidates].array ?? []
-                            let candidates: [BallotpediaElection.Candidate] = candidatesJson.map {
-                                let name = $0[c.person][c.name].stringValue
-                                let party = $0[c.party].array?.first?[c.name].stringValue
-                                let imageUrl = $0[c.person][c.image][c.url].stringValue
-                                //let websiteValue = $0[c.person][c.url].stringValue
-                                //let occupation = $0[c.person][c.url].stringValue
-                                return BallotpediaElection.Candidate(name: name,
-                                                                     party: party,
-                                                                     imageUrl: URL(string: imageUrl),
-                                                                     isIncumbent: "true",
-                                                                     socialMedia: "https://example.com",
-                                                                     website: "https://example.com",
-                                                                     occupation: "Mayor",
-                                                                     about: "...")
-                            }
-                            return BallotpediaElection.Race(name: name, level: level, candidates: candidates)
-                        }
-                        return BallotpediaElection.District(name: $0[c.name].stringValue, type: $0[c.type].stringValue, races: races)
-                    }
-                    guard let date = date else {
-                        return nil
-                    }
-                    return BallotpediaElection(date: date, districts: districts)
-
-                }
-                guard let encodedElectionInfo = try? JSONEncoder().encode(self?.electionInfo)
-                else {
-                    preconditionFailure("Failured to encode election info for UserDefaults.")
-                }
-                UserDefaults.standard.set(encodedElectionInfo, forKey: "electionInfo")
-
-                DispatchQueue.main.async {[weak self] in
-                    self?.electionInfo = elections
-                    self?.stateElections.reloadData()
-                }
-
-            }
-        }
-        else if UserDefaults.standard.string(forKey: "longitude") != nil && UserDefaults.standard.string(forKey: "latitude") != nil && self.homescreendata.count != 0 {
-            DispatchQueue.main.async {[weak self] in
-                self?.electionInfo = self!.homescreendata
-                self?.stateElections.reloadData()
-            }
-        }
-        else {
-            print("Error")
-        }
-
+        loadElectionData()
     }
     
     //Get api data for next election date
@@ -151,4 +78,114 @@ class HomeScreen: UIViewController {
 
 }
 
+//MARK: - API Networking
+private extension HomeScreen {
+    typealias c = Constants.JSON
 
+    private func makeRequest() -> Endpoint? {
+        guard UserDefaults.standard.string(forKey: "longitude") != nil &&
+                UserDefaults.standard.string(forKey: "latitude") != nil &&
+                homescreendata.count == 0
+        else {
+            return nil
+        }
+        let latitude = UserDefaults.standard.string(forKey: "latitude")
+        let longitude = UserDefaults.standard.string(forKey: "longitude")
+        let location = CLLocationCoordinate2D(latitude: CLLocationDegrees(Double(latitude!)!),
+                                              longitude: CLLocationDegrees(Double(longitude!)!))
+        let request = Endpoint.getAPI(from: .ballotpediaElectionInfo(location: location))
+        return request
+    }
+
+    private func loadElectionData() {
+        guard let request = makeRequest()
+        else {
+            if !homescreendata.isEmpty {
+                electionInfo = homescreendata
+                stateElections.reloadData()
+            }
+            return
+        }
+        URLSession.shared.codableTask(with: request) {[weak self] model in
+            let elections = self?.parseElections(model)
+
+            guard let encodedElectionInfo = try? JSONEncoder().encode(self?.electionInfo)
+            else {
+                preconditionFailure("Failured to encode election info for UserDefaults.")
+            }
+            UserDefaults.standard.set(encodedElectionInfo, forKey: "electionInfo")
+            DispatchQueue.main.async {[weak self] in
+                self?.electionInfo = elections ?? []
+                self?.stateElections.reloadData()
+            }
+
+        }
+    }
+
+    private func parseElections(_ model: JSON?) -> [BallotpediaElection] {
+        let electionsJson = model?[c.data][c.elections].array ?? []
+        let elections: [BallotpediaElection] = electionsJson.compactMap {election in
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            let date = formatter.date(from: election[c.date].stringValue)
+
+            guard let date = date else {
+                return nil
+            }
+
+            let districts = parseDistricts(election)
+            return BallotpediaElection(date: date, districts: districts)
+
+        }
+
+        return elections
+    }
+
+    private func parseDistricts(_ election: JSON) -> [BallotpediaElection.District] {
+        let districtsJson = election[c.districts].array ?? []
+        let districts: [BallotpediaElection.District] = districtsJson.compactMap {district in
+            let races = parseRaces(district)
+            return BallotpediaElection.District(name: district[c.name].stringValue, type: district[c.type].stringValue, races: races)
+        }
+
+        return districts
+    }
+
+    private func parseRaces(_ district: JSON) -> [BallotpediaElection.Race] {
+        let racesJson = district[c.races].array ?? []
+        let races: [BallotpediaElection.Race] = racesJson.compactMap {race in
+            let rawLevel = race[c.office][c.level].stringValue
+            let name = race[c.office][c.name].stringValue
+            guard let level = BallotpediaElection.ElectionLevel(rawValue: rawLevel.lowercased())
+            else {
+                return nil
+            }
+
+            let candidates = parseCandidates(race)
+            return BallotpediaElection.Race(name: name, level: level, candidates: candidates)
+        }
+
+        return races
+    }
+
+    private func parseCandidates(_ race: JSON) -> [BallotpediaElection.Candidate] {
+        let candidatesJson = race[c.candidates].array ?? []
+        let candidates: [BallotpediaElection.Candidate] = candidatesJson.map {candidate in
+            let name = candidate[c.person][c.name].stringValue
+            let party = candidate[c.party].array?.first?[c.name].stringValue
+            let imageUrl = candidate[c.person][c.image][c.url].stringValue
+            //let websiteValue = $0[c.person][c.url].stringValue
+            //let occupation = $0[c.person][c.url].stringValue
+            return BallotpediaElection.Candidate(name: name,
+                                                 party: party,
+                                                 imageUrl: URL(string: imageUrl),
+                                                 isIncumbent: "true",
+                                                 socialMedia: "https://example.com",
+                                                 website: "https://example.com",
+                                                 occupation: "Mayor",
+                                                 about: "...")
+        }
+
+        return candidates
+    }
+}
